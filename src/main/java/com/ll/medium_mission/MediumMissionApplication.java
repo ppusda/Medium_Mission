9b2843c8908b736171871506761adfb4ed059a97
ppusda <ppusda1234@gmail.com>
2023-12-08 23:08:44 +0900

    feat: init Project

---
+++ src/main/java/com/ll/medium_mission/MediumMissionApplication.java
@@ -0,0 +1,13 @@
+package com.ll.medium_mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediumMissionApplication.class, args);
+    }
+
+}
