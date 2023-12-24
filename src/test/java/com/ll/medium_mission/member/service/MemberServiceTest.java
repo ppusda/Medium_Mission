a8f84984294ec103f67fd57f83150f9892aa17f8
ppusda <ppusda1234@gmail.com>
2023-12-24 21:27:48 +0900

    fix: test 환경 변경

--- src/test/java/com/ll/medium_mission/member/service/MemberServiceTest.java
+++ src/test/java/com/ll/medium_mission/member/service/MemberServiceTest.java
@@ -14,9 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class MemberServiceTest {
 
     @Autowired
--- src/test/java/com/ll/medium_mission/post/controller/PostControllerTest.java
+++ src/test/java/com/ll/medium_mission/post/controller/PostControllerTest.java
@@ -21,9 +21,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("dev")
 class PostControllerTest {
 
     @Autowired
