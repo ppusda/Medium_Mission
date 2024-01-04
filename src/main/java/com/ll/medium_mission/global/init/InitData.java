4cdf212a74d746199eb28e479e7f93579b855553
ppusda <ppusda1234@gmail.com>
2024-01-04 16:23:10 +0900

    fix: prod 초기 데이터 설정

--- src/main/java/com/ll/medium_mission/global/init/InitData.java
+++ src/main/java/com/ll/medium_mission/global/init/InitData.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("!prod")
 @RequiredArgsConstructor
 public class InitData {
 
