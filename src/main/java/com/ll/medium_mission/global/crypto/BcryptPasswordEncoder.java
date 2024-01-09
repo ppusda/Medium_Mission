7a91f929bef079e751d96dceee6ce4957082382d
ppusda <ppusda1234@gmail.com>
2024-01-09 23:31:57 +0900

    fix: bean 중복 등록 이슈 해결

--- src/main/java/com/ll/medium_mission/global/crypto/BcryptPasswordEncoder.java
+++ src/main/java/com/ll/medium_mission/global/crypto/BcryptPasswordEncoder.java
@@ -3,12 +3,13 @@ package com.ll.medium_mission.global.crypto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class PasswordEncoder {
+public class BcryptPasswordEncoder {
 
     @Bean
-    org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
